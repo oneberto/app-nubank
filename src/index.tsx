0e662786650aa1ac9ca86dd515027d878190918b
Umberto Pontes <umberto.pontes@proenem.com.br>
2020-02-08 15:19:52 -0300

    First commit

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, StatusBar, Text } from 'react-native';
+
+const App = () => {
+    return (
+        <>
+            <StatusBar barStyle="dark-content" />
+            <View>
+                <Text>Hello</Text>
+            </View>
+        </>
+    );
+};
+
+export default App;
